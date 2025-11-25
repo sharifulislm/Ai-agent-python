8187f70e70e5dc188a7de03f6d6facb16255237c
sharifulislm <shariful.islam.coder@gmail.com>
2025-11-25 13:12:37 +0300

    git test

--- myapp/views.py
+++ myapp/views.py
@@ -5,5 +5,5 @@ from django.shortcuts import render
 # Create your views here.
 def machine(request):
 
-       return HttpResponse("I love suraiya")
+       return HttpResponse("I love suraiya she is my love")
 
